68a2ae7bbc72a987dccc1265808ead27134c97d0
dafisatria <satria1002@gmail.com>
2023-02-25 18:24:06 +0700

    add task on section 6

--- 07_Branching-Looping-Function/Praktikum/task_section_7/bin/task_section_7.dart
+++
@@ -1,5 +0,0 @@
-import 'package:task_section_7/task_section_7.dart' as task_section_7;
-
-void main(List<String> arguments) {
-  print('Hello world: ${task_section_7.calculate()}!');
-}
--- 07_Branching-Looping-Function/Praktikum/task_section_7/test/task_section_7_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:task_section_7/task_section_7.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
