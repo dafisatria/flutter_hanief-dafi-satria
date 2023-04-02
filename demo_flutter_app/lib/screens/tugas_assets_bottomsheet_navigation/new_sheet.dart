16ac8ad10dc17b51d7b4aff3fce2cc2c11a9d31e
dafisatria <satria1002@gmail.com>
2023-04-02 09:13:47 +0700

    added task in section 21

---
+++ demo_flutter_app/lib/screens/tugas_assets_bottomsheet_navigation/new_sheet.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ImageSheet extends StatelessWidget {
+  final parameter;
+  const ImageSheet({Key? key, required this.parameter}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Halo!'),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.popUntil(context, ModalRoute.withName('/'));
+          },
+          icon: const Icon(Icons.arrow_back_outlined),
+        ),
+      ),
+      body: Center(
+        child: Image.asset('$parameter'),
+      ),
+    );
+  }
+}
